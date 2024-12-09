55e8f60d834bfe3fbbef269912fed9e74b798783
CrisTinaMoran <khirviecliffordbautista@gmail.com>
2024-12-10 02:31:34 +0800

    Updated: 2:31-12.10.24

--- HGHMNDS/HGHMNDS_POS/settings.py
+++ HGHMNDS/HGHMNDS_POS/settings.py
@@ -104,10 +104,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR /  "HGHMNDS_App",  # Or replace with an absolute path
+    BASE_DIR /  "HGHMNDS_App/static",  # Or replace with an absolute path
 ]
 
 
