9cd7ba37cb2a797c8993f0d72f1399cc175a0713
CrisTinaMoran <khirviecliffordbautista@gmail.com>
2024-12-09 17:57:17 +0800

    Last Update: 5:57-12.9.2024

--- HGHMNDS/HGHMNDS_App/urls.py
+++ HGHMNDS/HGHMNDS_App/urls.py
@@ -3,8 +3,8 @@ from .import views
 
 urlpatterns = [
     path('', views.lobby, name='lobby'),
-    path('register/', views.register, name='register'),
     path('login/', views.Login_view, name='login'),
+    path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('seller-dashboard/', views.seller_dashboard, name='seller_dashboard'),
